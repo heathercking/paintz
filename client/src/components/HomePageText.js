5e83867f3f7f2ed4d69907865ec67213a4e39490
Heather King <hkingdevelopment@gmail.com>
2022-01-27 13:02:33 +0000

    adds search and convert buttons on homepage with onClick functions - useNavigate directs to the relevant component

---
+++ client/src/components/HomePageText.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePageText = () => {
+
+
+    return (
+        <>
+            <h2>This is the home page text</h2>
+        </>
+    )
+}
+
+export default HomePageText;
--- client/src/containers/HomepageContainer.js
+++ client/src/containers/HomepageContainer.js
@@ -1,6 +1,9 @@
 import React, {useState, useEffect} from "react";
 import App from "../App";
+import {useNavigate} from 'react-router-dom';
+
 import {getPaints} from '../services/PaintzService';
+import PaintSearchContainer from "./PaintSearchContainer";
 
 function HomePageContainer() {
 
@@ -13,9 +16,24 @@ function HomePageContainer() {
 
     console.log(paints);
 
+    let navSearch = useNavigate();
+    function handleSearchClick() {
+        navSearch('/search')
+    }
+
+    let navConverter = useNavigate();
+    function handleConverterClick() {
+        navConverter('/convert')
+    }
+
+
+
     return (
         <div>
             This is the home page
+            <button type="" onClick={handleSearchClick}>Search</button>
+            <button type="" onClick={handleConverterClick}>Converter</button>
+            
         </div>
     )
 }
