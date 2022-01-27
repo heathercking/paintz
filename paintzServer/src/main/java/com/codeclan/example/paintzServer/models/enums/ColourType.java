d36de746c34af15f3e6960a3a3fc9eb5db49ab67
Jean-Pierre Gazian <jeanpierregazian@msn.com>
2022-01-27 17:37:33 +0000

    adding data to DataLoader

--- paintzServer/src/main/java/com/codeclan/example/paintzServer/models/enums/ColourType.java
+++ paintzServer/src/main/java/com/codeclan/example/paintzServer/models/enums/ColourType.java
@@ -3,15 +3,14 @@ package com.codeclan.example.paintzServer.models.enums;
 public enum ColourType {
 
     RED,
-    RED_VIOLET,
-    BLUE_VIOLET,
+    VIOLET,
     BLUE,
-    BLUE_GREEN,
     GREEN,
-    YELLOW_GREEN,
     YELLOW,
-    YELLOW_ORANGE,
     ORANGE,
-    RED_ORANGE
+    BLACK,
+    WHITE,
+    GREY,
+    NEUTRAL
 
 }
